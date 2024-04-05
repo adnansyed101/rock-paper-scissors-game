0dfe1e280d4495fcba3e89838365b641f8567707
Md Adnan <sirgeant10@gmail.com>
2024-04-05 15:42:59 +0600

    Add getComputerChoice function
    This function will get a random choice from the choices array.

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+
+function getComputerChoice() {
+  const choices = ["Rock", "Paper", "Scissors"];
+  const randomNumber = Math.floor(Math.random() * choices.length);
+  return choices[randomNumber];
+}
+
+console.log(getComputerChoice());
