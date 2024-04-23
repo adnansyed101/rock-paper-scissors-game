ff8515b256f4fe17ad879dafc5246f2857755ec5
Md Adnan <sirgeant10@gmail.com>
2024-04-23 19:46:07 +0600

    Implement playgame function to play 5 rounds.
    the playgame function will play the game 5 times. and also validate if the player or computer has won.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const playerChoice = prompt("Rock, Paper or Scissor").toLowerCase();
-const computerChoice = getComputerChoice();
+let score = { player: 0, computer: 0 };
+console.log(playGame());
 
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissor"];
@@ -9,17 +9,38 @@ function getComputerChoice() {
 
 function playRound(player, computer) {
   if (player === "rock" && computer === "scissor") {
+    score.player += 1;
     return "Player Wins";
   } else if (player === "paper" && computer === "rock") {
+    score.player += 1;
     return "Player Wins";
   } else if (player === "scissor" && computer === "paper") {
+    score.player += 1;
     return "Player Wins";
   } else if (player === computer) {
     return "Draw";
   } else {
+    score.computer += 1;
     return "Computer Wins";
   }
 }
 
-console.log(`Player: ${playerChoice} \t Computer: ${computerChoice}`);
-console.log(playRound(playerChoice, computerChoice));
+function playGame() {
+  for (let i = 1; i <= 5; i++) {
+    const playerChoice = prompt("Rock, Paper or Scissor").toLowerCase();
+    const computerChoice = getComputerChoice();
+    console.log(`Round ${i}`);
+    console.log(`Player: ${playerChoice} \t Computer: ${computerChoice}`);
+    console.log(playRound(playerChoice, computerChoice));
+  }
+
+  if (score.player > score.computer) {
+    return "Player has won the game";
+  } else if (score.player < score.computer) {
+    return "Computer has won the game";
+  } else {
+    return "Draw";
+  }
+}
+
+console.log(score);
